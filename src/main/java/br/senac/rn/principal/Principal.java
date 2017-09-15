06b8ab92105994e52fe1c2d11f942d08b9e4228c
a9130439 <a9130439@02LAB108PC08.rn.senac.br>
2017-09-15 19:35:55 -0300

    Inserir cliente

--- src/main/java/br/senac/rn/principal/Principal.java
+++ src/main/java/br/senac/rn/principal/Principal.java
@@ -11,13 +11,13 @@ import br.senac.rn.model.Sexo;
 public class Principal {
     public static void main(String[] args) {
         
-//        Produto p1 = new Produto();
-//        p1.setNome("Pastel");
-//        p1.setDescricao("recheio de vento");
-//        p1.setPreco(10);
-//        p1.setCategoria(new CategoriaDAO().buscarPorId(1));
-//        ProdutoDAO dao = new ProdutoDAO();
-//        dao.inserir(p1);
+//      Produto p1 = new Produto();
+//      p1.setNome("Pastel");
+//      p1.setDescricao("recheio de vento");
+//      p1.setPreco(10);
+//      p1.setCategoria(new CategoriaDAO().buscarPorId(1));
+//      ProdutoDAO dao = new ProdutoDAO();
+//      dao.inserir(p1);
      
         Cliente cli = new Cliente();
         ClienteDAO dao = new ClienteDAO();
@@ -26,21 +26,21 @@ public class Principal {
         cli.setSexo(new SexoDAO().buscarPorId(1));
         dao.inserir(cli);
         
-//        CategoriaDAO dao = new CategoriaDAO();
-//        Categoria c1 = new Categoria();
-//        c1.setNome("vidro");
-//        c1.setDescricao("copos");
-//        dao.inserir(c1);
+//      CategoriaDAO dao = new CategoriaDAO();
+//      Categoria c1 = new Categoria();
+//      c1.setNome("vidro");
+//      c1.setDescricao("copos");
+//      dao.inserir(c1);
         
-    //    SexoDAO daoS = new SexoDAO();  
-    //    CategoriaDAO dao = new CategoriaDAO();
-    //
-    //    dao.atualizar(dao.buscarPorId(2));
-    //    Sexo s1 = new Sexo();       
-    //    s1.setNome("Masculino");
-    //    s1.setSigla('M');      
-    //    SexoDAO dao = new SexoDAO();        
-    //    dao.inserir(s1);        
+  //    SexoDAO daoS = new SexoDAO();  
+  //    CategoriaDAO dao = new CategoriaDAO();
+  //
+  //    dao.atualizar(dao.buscarPorId(2));
+  //    Sexo s1 = new Sexo();       
+  //    s1.setNome("Masculino");
+  //    s1.setSigla('M');      
+  //    SexoDAO dao = new SexoDAO();        
+  //    dao.inserir(s1);        
         System.exit(0);
     }
 }
