b87647890473ce9d7502a6cbc63e4f053d162f68
Alex <Alex@Alex>
2017-09-14 23:35:27 -0300

    adicionado a classe Cliente e a classe ClienteDAO

--- src/main/java/br/senac/rn/dao/ClienteDAO.java
+++ src/main/java/br/senac/rn/dao/ClienteDAO.java
@@ -1,40 +1,43 @@
 package br.senac.rn.dao;
 
 import br.senac.rn.model.Categoria;
+import br.senac.rn.model.Cliente;
 import br.senac.rn.model.Produto;
+import static com.sun.org.apache.xalan.internal.xsltc.compiler.util.Type.Int;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
-public class ProdutoDAO {
+public class ClienteDAO {
     private EntityManager manager;
     private EntityManagerFactory factory;
-    public ProdutoDAO(){
+
+    public ClienteDAO(){
         factory = Persistence.createEntityManagerFactory("ConexaoDB");
         manager = factory.createEntityManager();
     }
-    public void inserir(Produto produto){
+    public void inserir(Cliente cliente){
         manager.getTransaction().begin();
-        manager.persist(produto);
+        manager.persist(cliente);
         manager.getTransaction().commit();
     }
-    public void excluir(Produto produto){
+    public void excuir(Cliente cliente){
         manager.getTransaction().begin();
-        manager.remove(produto);
+        manager.remove(cliente);
         manager.getTransaction().commit();
     }
-    public void atualizar(Produto produto){
+    public void atualizar(Cliente cliente){
         manager.getTransaction().begin();
-        manager.merge(produto);
+        manager.merge(cliente);
         manager.getTransaction().commit();
     }
-    public List<Produto>buscarTodos(){
-        TypedQuery<Produto> consulta = manager.createQuery("SELECT p FROM Produto p", Produto.class);
+    public List<Cliente>buscarTodos(){
+        TypedQuery<Cliente> consulta = manager.createQuery"SELECT c FROM c", cliente.class);
         return consulta.getResultList();
     }
-    public Produto buscaPorId(int id){
-        return manager.find(Produto.class, id);
+    public Cliente buscaPorId(Int id){
+        return manager.find(Cliente.class, id);
     }
 }
---
+++ src/main/java/br/senac/rn/model/Cliente.java
@@ -0,0 +1,83 @@
+package br.senac.rn.model;
+
+public class Cliente {
+    private int id;
+    private String nome;
+    private int cpf;
+    private Sexo sexo;
+
+    public Cliente() {
+    }
+
+    public Cliente(int id, String nome, int cpf, Sexo sexo) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(Sexo sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 59 * hash + this.id;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" + "id=" + id + ", nome=" + nome + ", cpf=" + cpf + ", sexo=" + sexo + '}';
+    }
+    
+    
+    
+}
